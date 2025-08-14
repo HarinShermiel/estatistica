bca4c00e8f533a67e0109f97946244822241a0c6
EFG <efg@efg.local>
2025-08-13 21:22:44 -0300

    regressão multipla

--- correlacao/regressao.py
+++ correlacao/regressao.py
@@ -1,3 +1,4 @@
+# 1. Importação das libs
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,27 +6,68 @@ import seaborn as sns
 # pip install scikit-learn
 from sklearn.linear_model import LinearRegression
 
+# 2. Carregamento e preparação dos dados
 EXPOSICAO_ALGODAO = 'data/LungDisease.csv'
 dataframe = pd.read_csv(EXPOSICAO_ALGODAO)
 print(dataframe.head())
 
-dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
-plt.show()
+# Gráfico de DISPERSÃO
+# dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
+# plt.show()
 
-
-prefictors = ['Exposure']
+# 3. Configuração e treinamento do modelo
+# Define a variáve preditora (independente), que é
+# a Exposure e a variável de resultado que é o PEFR
+predictors = ['Exposure']
 outcome = 'PEFR'
-
+# Instanciar o modelo
 model = LinearRegression()
+# Etapa d etreinamento
+model.fit(dataframe[predictors], dataframe[outcome])
 
-model.fit(dataframe[prefictors], dataframe[outcome])
-
-print(f'Intercept: {model.intercept_:.3f}')
-
+# 4. Exibição dos Coeficiente
+# intercepto
+print(f'Intercepto: {model.intercept_:.3f}')
+# Coeficiente angular
 print(f'Coeficiente Angular: {model.coef_[0]}')
 
-fig, (reg) = plt.subplots(1, 1, figsize=(4, 4))
-
+# 5. Geração do gráfico
+fig, (reg, ax, res) = plt.subplots(1, 3, figsize=(12, 4))
+# Gráfico regreção
 reg = sns.regplot(x = 'Exposure', y = 'PEFR', data = dataframe, ax = reg)
+
+# Parcial
+# Definir os limites dos eixos X e Y
+ax.set_xlim(0, 23)
+ax.set_ylim(295, 450)
+# Definir os rótulos
+ax.set_xlabel('Exposure')
+ax.set_ylabel('PERF')
+# Plotar a linha
+ax.plot(dataframe['Exposure'], model.predict(dataframe[predictors]), '-')
+# Adicionar o texto b0
+ax.text(0.4, model.intercept_, r'$b_0$', size='larger')
+x = pd.DataFrame({'Exposure': [7.5,17.5]})
+y = model.predict(x)
+ax.plot((7.5, 7.5, 17.5), (y[0], y[1], y[1]), '--')
+# Exibir DEltaY e DeltaX no gráfico
+ax.text(5, np.mean(y), r'$\Delta Y$', size='larger')
+ax.text(12, y[1] - 10, r'$\Delta X$', size='larger')
+# Adicionar anotação para o coeficiente angular
+ax.text(12, 390, r'$b_1 = \frac{\Delta Y}{\Delta X}$', size='larger')
+
+# Valores ajustado e resíduos
+# Gera os valores ajustados do modelo
+fitted = model.predict(dataframe[predictors])
+# calcula os resíduos
+residuals = dataframe[outcome] - fitted
+# Exibe o gráfico de correlação
+res = dataframe.plot.scatter(x = 'Exposure', y = 'PEFR', ax = res)
+res.plot(dataframe.Exposure, fitted - 1)
+# Para cada valor de índice
+for x, yatual, yfitted in zip(dataframe.Exposure, dataframe.PEFR, fitted):
+    print(f'x: {x} - yreal: {yatual} - yreta: {yfitted}')
+    res.plot((x, x), (yatual, yfitted), '--', color='C1')
+
 plt.tight_layout()
 plt.show()
